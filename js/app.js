24226527e5d17ad6b5ef1e243280fe18a9ee6554
Mohiuddin Sikder <mohi.info20@gmail.com>
2021-09-13 16:27:26 +0600

    added card style

--- js/app.js
+++ js/app.js
@@ -18,16 +18,21 @@ const showProducts = (products) => {
         <div>
           <img class="product-image" src=${image}></img>
         </div>
-        <h3>${product.title}</h3>
+        <h4>${product.title}</h4>
         <p>Category: ${product.category}</p>
-        <h2>Price: $ ${product.price}</h2>
+        <h3>Price: $ ${product.price}</h3>
+        <div class="d-flex justify-content-between">
+          <p>Average Rate: ${product.rating.rate}</p>
+          <p>Count: ${product.rating.count}</p>
+        </div>
         <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-        <button id="details-btn" class="btn btn-danger">Details</button></div>
+        <button id="details-btn" onclick="details()" class="btn btn-danger">Details</button></div>
       `;
-      
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -80,3 +85,5 @@ const updateTotal = () => {
   getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+
